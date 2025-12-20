1c0512808166be5243e1e2417bfa8271c8c675aa
Anthony Gayflor <devgayflor@gmail.com>
2025-12-20 17:38:56 -0500

    Remove socket-server directory and all associated files for a complete project cleanup. This includes the removal of configuration files, source code, database schemas, and handler implementations, streamlining the codebase and eliminating unused components.

--- socket-server/src/types/socketio.ts
+++
@@ -1,9 +0,0 @@
-import { Session, User } from "better-auth";
-import { Socket } from "socket.io";
-
-export interface SocketWithUser extends Socket {
-  data: {
-    user: User;
-    session: Session;
-  };
-}
